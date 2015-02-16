6f1e73142437f2529da357dfa549868e15b46840
Devon Hollowood <devonhollowood@gmail.com>
2015-02-15 17:21:50 -0800

    Wrote final version of NPriorityQueue

--- 2015-02-11/npqtest.cpp
+++ 2015-02-11/npqtest.cpp
@@ -4,25 +4,57 @@
 
 using namespace std;
 
-void fill(NPriorityQueue<char, 3, float> npq){
-    npq.enqueue('a', {1,2,3});
-    npq.enqueue('b', {2,3,1});
-    npq.enqueue('b', {3,1,2});
+void fill(NPriorityQueue<char, 3, float>& npq){
+    npq.enqueue('A', {{20, 2,19}});
+    npq.enqueue('A', {{26,10,28}});
+    npq.enqueue('C', {{15,30,11}});
+    npq.enqueue('D', {{19, 1,18}});
+    npq.enqueue('E', {{14,27,16}});
+    npq.enqueue('E', {{18,23,31}});
+    npq.enqueue('F', {{12,17, 8}});
+    npq.enqueue('G', {{ 9, 9,26}});
+    npq.enqueue('G', {{29,16, 6}});
+    npq.enqueue('I', {{31,14, 2}});
+    npq.enqueue('I', {{ 7,25, 4}});
+    npq.enqueue('I', {{27,29,20}});
+    npq.enqueue('K', {{ 6, 4, 3}});
+    npq.enqueue('K', {{16,31,12}});
+    npq.enqueue('L', {{10,11,15}});
+    npq.enqueue('L', {{17,24,21}});
+    npq.enqueue('M', {{21,26,29}});
+    npq.enqueue('M', {{32, 8,30}});
+    npq.enqueue('N', {{ 8,15, 5}});
+    npq.enqueue('O', {{22,18, 7}});
+    npq.enqueue('O', {{ 5, 6,10}});
+    npq.enqueue('O', {{ 3, 7,25}});
+    npq.enqueue('P', {{ 1,20,23}});
+    npq.enqueue('R', {{ 2, 3,24}});
+    npq.enqueue('R', {{13, 5, 9}});
+    npq.enqueue('R', {{24,19,27}});
+    npq.enqueue('R', {{28,22,32}});
+    npq.enqueue('R', {{30,28,13}});
+    npq.enqueue('S', {{25,32,17}});
+    npq.enqueue('U', {{23,21,14}});
+    npq.enqueue('V', {{ 4,12, 1}});
+    npq.enqueue('Y', {{11,13,22}});
 }
 
 template<size_t N>
-void empty(NPriorityQueue<char, 3, float> npq){
+void empty(NPriorityQueue<char, 3, float>& npq){
+    cout << "count = " << npq.count() << endl;
     while(npq.count()!=0){
-        cout << npq.dequeue<N>() << endl;
+        cout << npq.dequeue<N>() << flush;
     }
+    cout << endl;
+    cout << "count = " << endl;
 }
 
 int main(){
     NPriorityQueue<char, 3, float> npq;
     fill(npq);
+    empty<0>(npq);
+    fill(npq);
     empty<1>(npq);
     fill(npq);
     empty<2>(npq);
-    fill(npq);
-    empty<3>(npq);
 }
--- 2015-02-11/npriorityqueue.hpp
+++ 2015-02-11/npriorityqueue.hpp
@@ -4,6 +4,7 @@
 #include <utility>
 #include <algorithm>
 #include <functional>
+//#include <iostream> //debug
 
 template <typename ValType, size_t NQueues, typename PriorityType>
 class NPriorityQueue{
@@ -13,19 +14,25 @@ public:
 
     //functions
     NPriorityQueue& enqueue(ValType value, PriorityList priorities){
-        elements_.push_front(value, priorities);
-        for(size_t N=0; N<priorities.size(); ++N){
+        //std::cout << "Enqueueing " << value << std::endl; //debug
+        elements_.push_front({value, priorities});
+        for(size_t N=0; N<priorities_.size(); ++N){
             insert_sorted_(N, elements_.begin());
         }
         return *this;
     }
     template<size_t N> ValType&& dequeue(){
         static_assert(N<NQueues, "dequeue()'s N out of bounds.");
-        auto element = *priorities_[N].front();
+        //std::cout << "dequeueing " << N << std::endl; //debug
+        auto pelement = priorities_[N].front();
+        //std::cout << "...element is " << pelement->first << std::endl; //debug
         for(size_t listN = 0; listN<priorities_.size(); ++listN){
-            auto to_erase = find_sorted_(N, element);
-            priorities_[N].erase(to_erase);
+            auto to_erase = find_sorted_(listN, pelement);
+            //std::cout << "...erasing" << std::endl; //debug
+            priorities_[listN].erase(to_erase);
         }
+        elements_.erase(pelement);
+        return std::move(pelement->first);
     }
     size_t count(){return elements_.size();}
     void clear(){elements_.clear(); priorities_.clear();}
@@ -39,24 +46,31 @@ private:
     //functions
     std::function<bool(const pElement&, const pElement&)> compare_(size_t N) const{
         return [N](const pElement& a, const pElement& b){
-            return a->second[N] < b->second[N];
+            PriorityType ap = a->second[N];
+            PriorityType bp = b->second[N];
+            return ap<bp;
         };
     }
 
     void insert_sorted_(size_t N, pElement ptr){
-        auto list = priorities_[N];
-        auto position = std::lower_bound(list.cbegin(), list.cend(), 
+        //std::cout << "...inserting " << ptr->first << " into " << N //debug
+            //<< " with priority " << ptr->second[N] << std::endl; //debug
+        auto& list = priorities_[N];
+        auto position = std::upper_bound(list.begin(), list.end(), ptr,
                 compare_(N));
-        priorities_.at(N).insert(position, ptr);
+        priorities_[N].insert(position, ptr);
     }
 
-    PriorityListItr find_sorted_(size_t N, Element el) const{
-        auto list = priorities_[N];
-        auto range = std::equal_range(list.begin(), list.end(), 
+    PriorityListItr find_sorted_(size_t N, pElement pel){
+        //std::cout << "...finding in " << N << std::endl; //debug
+        auto& list = priorities_[N];
+        auto range = std::equal_range(list.begin(), list.end(), pel, 
                 compare_(N));
         for(auto itr=range.first; itr!=range.second; ++itr){
-            if(**itr==el) return itr;
+            //if(*itr==pel) std::cout << "...found." << std::endl; //debug
+            if(*itr==pel) return itr;
         }
+        //std::cout << "...not found" << std::endl; //debug
         return list.end(); //if not found
     }
 
