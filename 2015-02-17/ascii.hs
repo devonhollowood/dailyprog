108528c0637489926c712399032b7690cf5d0f5f
Devon Hollowood <devonhollowood@gmail.com>
2015-02-17 14:11:56 -0800

    Added 2015-02-17 challenge: ascii.hs

---
+++ 2015-02-17/ascii.hs
@@ -0,0 +1,11 @@
+import System.Environment (getArgs)
+import Data.Char (chr)
+import Data.List.Split (chunksOf)
+
+readBin = foldl step 0
+    where l `step` r = l*2 + to_int r
+          to_int = read . return
+
+toAscii = map chr . map readBin . chunksOf 8
+
+main = getArgs >>= mapM_  (putStrLn . toAscii)
