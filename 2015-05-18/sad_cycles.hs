5fc899f280033a895b351e317ee1ea3520a80724
Devon Hollowood <devonhollowood@gmail.com>
2015-05-18 17:18:04 -0700

    Cleaned up sad_cycles.hs

--- 2015-05-18/sad_cycles.hs
+++ 2015-05-18/sad_cycles.hs
@@ -1,25 +1,19 @@
-import Data.Char (intToDigit, digitToInt)
+import Data.Char (digitToInt)
 import Data.List (intercalate)
-
-digits :: Integer -> [Int]
-digits = map digitToInt . show
-
-undigits :: [Int] -> Integer
-undigits = foldl acc 0
-    where acc m n = 10*m + toInteger n
-
-nextSad :: Integer -> Integer -> Integer
-nextSad b = sum . map ((^b) . toInteger) . digits
-
-runCycle b n l
-    | next `elem` l = reverse $ next : takeWhile (/=next) l
-    | otherwise     = runCycle b next (next:l)
-    where next = nextSad b n
-
-sadCycle b n = runCycle b n []
 
 main = do
     b <- readLn :: IO Integer
     n <- readLn :: IO Integer
     let cycle = sadCycle b n
     putStrLn $ intercalate ", " $ map show cycle
+
+sadCycle b n = runCycle b n []
+
+runCycle b n l
+    | next `elem` l = reverse $ next : takeWhile (/=next) l
+    | otherwise     = runCycle b next (next:l)
+    where next = nextSad b n
+
+nextSad b = sum . map ((^b) . toInteger) . digits
+
+digits = map digitToInt . show
