9b5ec3178d08bf3118d5ddd2dc36112345628058
Devon Hollowood <devonhollowood@gmail.com>
2015-06-08 01:43:30 -0700

    Continued WiP for latex.hs

--- 2015-06-05/latex.hs
+++ 2015-06-05/latex.hs
@@ -12,24 +12,25 @@ main = do
     contents <- IO.readFile infile
     case parse latex "" contents of
         Left err -> putStrLn $ show err
-        Right lc -> B.printBox $ chunkBox lc
+        Right bx -> B.printBox bx
 
-latex :: Parser LatexChunk
+latex :: Parser Box
 latex = do
     chunks <- many latexChunk
-    return $ mconcat chunks
+    return . toBox $ mconcat chunks
 
 latexChunk :: Parser LatexChunk
 latexChunk = command <|> plaintex
 
 command :: Parser LatexChunk
-command = backSlashCommand <|> subscript <|> superscript
+command = backSlashCommand <|> try subsuper <|> subscript <|> superscript
+          <?> "valid command"
 
-arg = char '{' *> latex <* char '}' >>= return . chunkBox
+arg = between (char '{') (char '}') latex <?> "bracketed argument"
 
 backSlashCommand = do
     char '\\'
-    frac <|> psqrt <|> root <|> ppi
+    frac <|> psqrt <|> root <|> ppi <?> "backslash command"
 
 frac = do
     string "frac" 
@@ -37,43 +38,53 @@ frac = do
     denom <- arg
     let width = max (B.cols num) (B.cols denom)
     let new_box = B.vcat B.center1 [num, B.text (replicate width '-'), denom]
-    return $ LatexChunk Inline new_box
+    return $ chunkInLine new_box
 
 psqrt = do
     string "sqrt"
     contents <- arg
     let width = B.cols contents
-    let height = B.cols contents
-    let underroot = B.vcat B.center1 [contents, B.text (replicate width '_')]
-    let new_box = B.hcat B.bottom [makeRadical height, underroot]
-    return $ LatexChunk Inline new_box
+    let height = B.rows contents
+    let ceiling = B.text (replicate width '_')
+    let aligned_ceil = B.alignHoriz B.right (width + height) ceiling
+    let radical = makeRadical height
+    return $ chunk aligned_ceil (B.hcat B.center1 [radical, contents]) B.nullBox
 
 root = do
     string "root"
     power <- arg
     contents <- arg
     let width = B.cols contents
-    let height = B.cols contents
-    let underroot = B.vcat B.center1 [contents, B.text (replicate width '_')]
-    let radical = B.vcat B.right [power, makeRadical height]
-    let new_box = B.hcat B.bottom [radical, underroot]
-    return $ LatexChunk Inline new_box
+    let height = B.rows contents
+    let ceiling = B.text (replicate width '_')
+    let radical = makeRadical height
+    return $ chunk (B.hcat B.center1 [B.alignHoriz B.right height power, 
+                                      ceiling])
+                   (B.hcat B.center1 [radical, contents])
+                   B.nullBox
 
-ppi = string "pi" *> return (LatexChunk Inline (B.char '\x03c0'))
+ppi = string "pi" *> return (chunkInLine (B.char '\x03c0'))
+
+subsuper = do
+    char '_'
+    sub <- arg
+    char '^'
+    super <- arg
+    return $ chunk super B.nullBox sub
 
 subscript = do
     char '_'
     sub <- arg
-    return $ LatexChunk Below sub
+    return $ chunkBelow sub
 
 superscript = do
     char '^'
     super <- arg
-    return $ LatexChunk Above super
+    return $ chunkAbove super
 
 plaintex = do
-    contents <- many $ noneOf "\\_^"
-    return $ LatexChunk Inline (B.text contents)
+    contents <- many1 $ noneOf "\\_^{}"
+    return $ chunkInLine (B.text $ filter (/= '\n') contents)
 
 makeRadical 1 = B.char '\x221a'
 makeRadical height = B.hcat B.top [B.moveDown 1 $ makeRadical (height-1),
@@ -82,25 +93,27 @@ makeRadical height = B.hcat B.top [B.moveDown 1 $ makeRadical (height-1),
 data ChunkPosition = Inline | Above | Below
 
 data LatexChunk = LatexChunk {
-    chunkPos :: ChunkPosition,
-    chunkBox :: Box
+    above  :: Box,
+    inLine :: Box,
+    below  :: Box
 }
 
---TODO: Add rules for Above Below mixing, change associativity
-
 instance Monoid LatexChunk where
-    mempty = LatexChunk Inline B.nullBox
-    c1 `mappend` c2 = let new_pos = chunkPos c2 in
-        case chunkPos c1 of
-            Inline -> LatexChunk new_pos $
-                          B.hcat B.center1 [chunkBox c1, chunkBox c2]
-            Above -> let height  = B.rows (chunkBox c2)
-                         new_box = B.hcat B.bottom
-                                       [B.moveUp height (chunkBox c1),
-                                        chunkBox c2]
-                         in LatexChunk new_pos new_box
-            Below -> let height  = B.rows (chunkBox c2)
-                         new_box = B.hcat B.top
-                                       [B.moveDown height (chunkBox c1),
-                                        chunkBox c2]
-                         in LatexChunk new_pos new_box
+    mempty = LatexChunk B.nullBox B.nullBox B.nullBox
+    mappend a b = let combine f = B.hcat B.center1 [f a, f b]
+                  in LatexChunk (combine above) (combine inLine) (combine below)
+
+chunk :: Box -> Box -> Box -> LatexChunk
+chunk top mid bot = let width = maximum $ map B.cols [top, mid, bot]
+                        align = B.alignHoriz B.left width
+                    in LatexChunk (align top) (align mid) (align bot)
+
+chunkAbove :: Box -> LatexChunk
+chunkAbove top = chunk top B.nullBox B.nullBox
+chunkInLine :: Box -> LatexChunk
+chunkInLine mid = chunk B.nullBox mid B.nullBox
+chunkBelow :: Box -> LatexChunk
+chunkBelow bot = chunk B.nullBox B.nullBox bot
+
+toBox :: LatexChunk -> Box
+toBox lc = B.vcat B.left [above lc, inLine lc, below lc]
